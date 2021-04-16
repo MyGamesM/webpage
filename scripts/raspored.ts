f4b82e3306bb52ab58e3302c8dba3e3392ed96bd
MyGamesM <milanmilan.mm55@gmail.com>
2021-04-16 09:41:01 +0200

    .

---
+++ scripts/raspored.ts
@@ -0,0 +1,5 @@
+// getComputedStyle(document.documentElement)
+//     .getPropertyValue('--my-variable-name');
+// document.documentElement.style
+//     .setProperty('--my-variable-name', 'pink');
+document.getElementById("p1").innerHTML = Date();
