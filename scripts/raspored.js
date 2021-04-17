50584b92c6e95a958d890dbb5e92c1a27a420821
MyGamesM <milanmilan.mm55@gmail.com>
2021-04-17 15:54:03 +0200

    raspored

--- scripts/raspored.js
+++ scripts/raspored.js
@@ -3,39 +3,65 @@ var loc = [['Matematika', 'Biologija', 'Fizizka', 'Srpski', 'Muzicko', 'Nemacki'
 var lost = ['08:35', '09:10', '09:55', '10:30', '11:05', '11:40', '12:15'];
 var loet = ["09:05", "09:40", "10:25", "11:00", "11:35", "12:10", "12:45"];
 function fillin(x) {
+    togglevisible(x);
     if (x < 0 || x > 5)
         return;
-    if (x != 1)
-        togglevisible();
     for (let i = 0; i < loc[n - 1].length; i++) {
         document.getElementById(`p${i + 1}`).innerHTML = loc[n - 1][i];
         document.getElementById(`a${i + 1}`).innerHTML = lost[i];
         document.getElementById(`b${i + 1}`).innerHTML = loet[i];
     }
 }
-function togglevisible() {
+function togglevisible(a) {
     let x = "";
-    for (let i = 0; i < 3; i++) {
-        console.log(i);
-        switch (x) {
-            case "":
-                x = "a";
-                break;
-            case "a":
-                x = "b";
-                break;
-            case "b":
-                x = "p";
-                break;
-            default:
-                break;
+    if (a == 0 || a == 6) {
+        for (let i = 0; i < 3; i++) {
+            switch (x) {
+                case "":
+                    x = "a";
+                    break;
+                case "a":
+                    x = "b";
+                    break;
+                case x = "b":
+                    x = "p";
+                    break;
+                default:
+                    break;
+            }
+            for (let j = 0; j < 7; j++) {
+                var element = document.getElementById(`${x}${j + 1}`);
+                if (element.style.display === "none") {
+                    element.style.display = "block";
+                }
+                else {
+                    element.style.display = "none";
+                }
+            }
         }
-        var element = document.getElementById(`${x}7`);
-        if (element.style.display === "none") {
-            element.style.display = "block";
-        }
-        else {
-            element.style.display = "none";
+    }
+    if (a == 0 || a == 6)
+        return;
+    if (a != 1) {
+        for (let i = 0; i < 3; i++) {
+            switch (x) {
+                case "":
+                    x = "a";
+                    break;
+                case "a":
+                    x = "b";
+                    break;
+                case "b":
+                    x = "p";
+                    break;
+                default:
+                    break;
+            }
+            var element = document.getElementById(`${x}7`);
+            if (element.style.display === "none")
+                element.style.display = "block";
+            else
+                element.style.display = "none";
         }
     }
 }
--- scripts/raspored.ts
+++ scripts/raspored.ts
@@ -5,9 +5,9 @@ var lost = ['08:35', '09:10', '09:55', '10:30', '11:05', '11:40', '12:15']
 var loet = ["09:05", "09:40", "10:25", "11:00", "11:35", "12:10", "12:45"]
 
 function fillin(x:number) {
-    if (x < 0 || x > 5) return;
+    togglevisible(x);
 
-    if (x != 1) togglevisible();
+    if (x < 0 || x > 5) return;
     
     for (let i = 0; i < loc[n-1].length; i++) {
         document.getElementById(`p${i+1}`).innerHTML = loc[n-1][i];
@@ -16,32 +16,57 @@ function fillin(x:number) {
     }
 }
 
-function togglevisible() {
+function togglevisible(a:number) {
     let x:string = "";
 
-    for (let i = 0; i < 3; i++) {
-        console.log(i);
-
-        switch (x) {
-            case "":
-                x = "a";
-                break;
-            case "a":
-                x = "b";
-                break;
-            case "b":
-                x = "p";
-                break;
-            default:
-                break;
+    if (a == 0 || a == 6) {
+        for (let i = 0; i < 3; i++) {
+            switch (x) {
+                case "":
+                    x = "a";
+                    break;
+                case "a":
+                    x = "b";
+                    break;
+                case x = "b":
+                    x = "p";
+                    break
+                default:
+                    break;
+            }
+            
+            for (let j = 0; j < 7; j++) {
+                var element = document.getElementById(`${x}${j+1}`);
+                if (element.style.display === "none") {
+                    element.style.display = "block";
+                }
+                else {
+                    element.style.display = "none";
+                }
+            }
         }
+    }
 
-        var element = document.getElementById(`${x}7`);
-        if (element.style.display === "none") {
-            element.style.display = "block";
-        }
-        else {
-            element.style.display = "none";
+    if (a == 0 || a == 6) return
+    if (a != 1) {
+        for (let i = 0; i < 3; i++) {
+            switch (x) {
+                case "":
+                    x = "a";
+                    break;
+                case "a":
+                    x = "b";
+                    break;
+                case "b":
+                    x = "p";
+                    break;
+                default:
+                    break;
+            }
+    
+            var element = document.getElementById(`${x}7`);
+            if (element.style.display === "none") element.style.display = "block";
+            else element.style.display = "none";
         }
     }
 }
