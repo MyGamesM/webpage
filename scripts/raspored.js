5874cf90dbbcb44677137ca9d6627204fa9e9c0d
MyGamesM <milanmilan.mm55@gmail.com>
2021-09-13 19:13:32 +0200

    updated loc list

--- scripts/raspored.js
+++ scripts/raspored.js
@@ -1,6 +1,6 @@
 var n = new Date().getDay();
 //var loc = [['Matematika', 'Biologija', 'Fizizka', 'Srpski', 'Muzicko', 'Nemacki', 'Odeljenska'], ['Srpski', 'Fizicko', 'Matematika', 'Hemija', 'Istorija', 'Biologija'], ['Fizika', 'Engleski', 'Srpski', 'Hemija', 'Istorija', 'Fizicko'], ['Likovno', 'Geografija', 'Nemacki', 'Srpski', 'Matematika', 'Engleski'], ['Informatika', 'Matematika', 'TIT', 'TIT', 'Geografija', 'Fizicko']];
-var loc = [["Matematika", "Engleski", "Likovno", "Srpski", "OET", "Fizicko", "Verska"], ["Fizika", "Matematika", "Praksa/Prog", "Praksa/Prog", "Engleski", "Inform/OET", "Inform/Null"], ["Istorija", "Fizika", "Hemija", "Geografija", "OET", "Gradjan/Null", "Null/Gradjan"], ["Geografija", "Fizicko", "Srpski", "Srpski", "Praksa/RG", "Praksa/RG", "Hemija"], ["RG/Infrom", "RG/Infrom", "Istorija", "Matematika", "OET", "OET/Prog", "Null/Prog"]];
+var loc = [["Matematika", "Likovno", "Engleski", "Srpski", "OET", "Fizicko", "Verska"], ["Matematika", "Fizika", "Prog/Praksa", "Prog/Praksa", "Engleski", "RiI/OET", "RiI/Null"], ["Istorija", "Hemija", "Fizika", "Geografija", "OET", "Gradjan/Null", "Null/Gradjan"], ["Geografija", "Fizicko", "Srpski", "Srpski", "Praksa/RG", "Praksa/RG", "Hemija"], ["RG/RiI", "RG/RiI", "Istorija", "Matematika", "OET", "OET/Prog", "Null/Prog"]];
 var lost = ["08:00", "08:55", "10:00", "10:55", "11:50", "12:45", "13:35"];
 var loet = ["08:45", "09:40", "10:45", "11:40", "12:35", "13:30", "14:20"];
 function fillin(x, classes /*, timestart:string[], timeend:string[]*/) {
