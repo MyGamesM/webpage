65a3c5b8e6ae7dc129dd63e9c8aa30780998bc98
MyGamesM <milanmilan.mm55@gmail.com>
2022-03-03 19:15:46 +0100

    Raspored

--- scripts/raspored.ts
+++ scripts/raspored.ts
@@ -3,10 +3,10 @@ var n = new Date().getDay();
 //var loc = [['Matematika', 'Biologija', 'Fizizka', 'Srpski', 'Muzicko', 'Nemacki', 'Odeljenska'], ['Srpski', 'Fizicko', 'Matematika', 'Hemija', 'Istorija', 'Biologija'], ['Fizika', 'Engleski', 'Srpski', 'Hemija', 'Istorija', 'Fizicko'], ['Likovno', 'Geografija', 'Nemacki', 'Srpski', 'Matematika', 'Engleski'], ['Informatika', 'Matematika', 'TIT', 'TIT', 'Geografija', 'Fizicko']];
 var loc = [
 	["Engleski", "OET", "Likovno", "Fizicko", "Matematika", "OET"],
-	["Hemija", "Istorija", "Program", "Program", "Engleski", "Inform", "Inform"],
-	["Matematika", "Fizika", "Hemija", "Geografija", "OET", "Gradjasko"],
-	["Fizika", "Fizicko", "Matematika", "Geografija", "Praksa", "Praksa", "Srpski"],
-	["RacunarGraf", "RacunarGraf", "Istorija", "OET", "Srpski", "Srpski"]
+	["Srpski", "Istorija", "Program", "Program", "Engleski", "Inform", "Inform"],
+	["Srpski", "Fizika", "Hemija", "Geografija", "OET", "Gradjasko"],
+	["Fizika", "Fizicko", "Matematika", "Geografija", "Praksa", "Praksa", "Hemija"],
+	["RacunarGraf", "RacunarGraf", "Istorija", "OET", "Matematika", "Srpski"]
 ]
 var lost = ["08:00", "08:55", "10:00", "10:55", "11:50", "12:45", "13:35"]
 var loet = ["08:45", "09:40", "10:45", "11:40", "12:35", "13:30", "14:20"]
