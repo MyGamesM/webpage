a94396cbb7b8a95f13a064f847a124007aef36e0
MyGamesM <milanmilan.mm55@gmail.com>
2023-02-23 15:42:55 +0100

    squares

---
+++ scripts/squares.js
@@ -0,0 +1,29 @@
+const main = document.getElementById('main')
+
+function createTable() {
+    for (let i = 0; i < 9; i++) {
+		for (let j = 0; j < 9; j++) {
+            main.appendChild(createRow(i, j))
+		}
+    }
+}
+
+function createRow(i, j) {
+    const element = document.createElement('div')
+    let color = i + j;
+
+	if (color >= 9) { color = invert(color - 9, 7); }
+	
+	element.className = `square c${color}`
+    
+	return element
+}
+
+function invert(a, p) {
+	let res = p - a
+
+	if (res < 0) { return res }
+	else { return res }
+}
+
+createTable()
